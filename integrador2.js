a5ca65b65649f0b5d700e32e945c19ddc8a15950
a20160052 <131193133+a20160052@users.noreply.github.com>
2023-09-19 12:42:40 -0500

    Add files via upload

---
+++ integrador2.js
@@ -0,0 +1,22 @@
+nom=prompt("Esta es la trivia de primavera, escriva su nombre")
+preg1=prompt("¿Cómo se llama al primer día de la Primavera?\nA: Primer Día de la Primavera\nB: Equinoccio de Primavera\nC: Equinoccio de Invierno")
+if(preg1=="B"){
+    preg1=25
+}
+else{
+    preg1=0
+}
+preg2=prompt("¿Qué sucede en el Polo Norte cuando llega la Primavera?\nA: 6 meses de luz diurna ininterrumpida\nB: 6 meses de oscuridad inimterrumpida\nC: 6 meses de alternancia de luz y oscuridad")
+if(preg2=="B"){
+    preg2=25
+}
+else{
+    preg2=0
+}
+total=preg1 + preg2
+if (total>=50){
+document.write(nom," tu puntaje es ", total, " y sabes sobre la primavera ")
+}
+else{ 
+document.write(nom," tu puntaje es ", total,  " te falta conocimiento ")
+}
